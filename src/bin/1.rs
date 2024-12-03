c895bdcdef9c453b1acd23488de5596018b27a58
Zygimantas Vanagas <zygimantux@gmail.com>
2024-12-03 22:41:09 +0200

    Initial commit

---
+++ src/bin/1.rs
@@ -0,0 +1,39 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let file_content = match read_to_string("./inputs/1.txt") {
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            return;
+        }
+    };
+
+    let mut left_map = Vec::new();
+    let mut right_map = Vec::new();
+
+    for line in file_content.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if let (Ok(left), Ok(right)) = (parts[0].parse::<i32>(), parts[1].parse::<i32>()) {
+            left_map.push(left);
+            right_map.push(right);
+        } else {
+            eprintln!("Failed to parse numbers in line: {}", line);
+        }
+    }
+
+    left_map.sort();
+    right_map.sort();
+
+    let length = left_map.len();
+    let mut result = 0;
+
+    for i in 0..length {
+        let left = left_map[i];
+        let right = right_map[i];
+        let difference = (left - right).abs();
+        result += difference; // Add to the result
+    }
+
+    println!("Result: {}", result);
+}
