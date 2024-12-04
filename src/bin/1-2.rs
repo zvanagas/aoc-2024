8701a4c4a77bac7d003f51f12137fb0eed46e426
Žygimantas Vanagas <zygimantux@gmail.com>
2024-12-04 12:18:26 +0200

    Add day 1 part 2

--- src/bin/1-2.rs
+++ src/bin/1-2.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{collections::BTreeMap, fs::read_to_string};
 
 fn main() {
     let file_content = match read_to_string("./inputs/1.txt") {
@@ -10,29 +10,26 @@ fn main() {
     };
 
     let mut left_map = Vec::new();
-    let mut right_map = Vec::new();
+    let mut right_map = BTreeMap::new();
 
     for line in file_content.lines() {
         let parts: Vec<&str> = line.split_whitespace().collect();
         if let (Ok(left), Ok(right)) = (parts[0].parse::<i32>(), parts[1].parse::<i32>()) {
             left_map.push(left);
-            right_map.push(right);
+            *right_map.entry(right).or_insert(0) += 1;
         } else {
             eprintln!("Failed to parse numbers in line: {}", line);
         }
     }
 
     left_map.sort();
-    right_map.sort();
 
-    let length = left_map.len();
     let mut result = 0;
 
-    for i in 0..length {
-        let left = left_map[i];
-        let right = right_map[i];
-        let difference = (left - right).abs();
-        result += difference;
+    for left in left_map {
+        let occurrences = right_map.get(&left).cloned().unwrap_or(0);
+        let total = left * occurrences;
+        result += total;
     }
 
     println!("Result: {}", result);
